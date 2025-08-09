74bd9512322b1a47500f825177898e7108bc5cc8
Tan Phan <tan@dataxight.com>
2025-08-09 13:51:17 +0700

    use anndata interface for reading

--- scripts/test-pbmc.py
+++ scripts/test-pbmc.py
@@ -12,10 +12,10 @@ apply_flotilla_patches()
 def main():
     daft.context.set_runner_ray()
     #daft.context.set_execution_config(scan_tasks_min_size_bytes=100000, min_cpu_per_task=1)
-    df = read_h5ad("/Users/tanphan/Downloads/pbmc_seurat_v4.h5ad", batch_size=5000, preview_size=20)
+    df = read_h5ad("/Users/tanphan/Downloads/pbmc_seurat_v4.h5ad", batch_size=5000, preview_size=0)
     start = time.time()
     #df.count().show()
-    df.write_parquet("pbmc-test")
+    df.write_parquet("pbmc-full")
     print(f"Time took: {time.time() - start}")
 
 if __name__ == "__main__":
