b62d6612f6a4b427b9827a22186bc7aa214c3469
tan <tan@dataxight.com>
2025-09-07 06:57:42 +0000

    update perturb dataset

--- src/protoplast/scrna/anndata/dataloader.py
+++ src/protoplast/scrna/anndata/dataloader.py
@@ -5,6 +5,9 @@ import anndata as ad
 import numpy as np
 from collections import defaultdict
 import logging
+
+from protoplast.scrna.train.utils import make_onehot_encoding_map
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -27,12 +30,30 @@ class PerturbDataset(Dataset):
         - b: batch label (int index, optional)
         - xp: perturbation target (int index, gene id or "control")
     """
-    def __init__(self, h5ad_files, control_label="non-targeting", use_batches=True, device="cpu"):
+    def __init__(
+        self,
+        h5ad_files: list[str],
+        pert_embedding_file: str, # TODO: use onehot encoding if this file is not provided
+        cell_type_label: str = "cell_type",
+        target_label: str = "target_gene", 
+        control_label: str = "non-targeting",
+        batch_label: str = "batch_var", 
+        use_batches: bool = True,
+        n_basal_samples: int = 30,
+        device: str = "cpu",
+    ):
         self.control_label = control_label
+        self.target_label = target_label
+        self.cell_type_label = cell_type_label
+        self.batch_label = batch_label
         self.use_batches = use_batches
         self.device = device
         self.h5ad_files = h5ad_files
         self.sp_adatas = []
+        self.pert_embedding_file = pert_embedding_file
+        self.n_basal_samples = n_basal_samples
+
+        self.pert_embedding = torch.load(pert_embedding_file)
 
         # Load and concatenate AnnData objects
         for i,f in enumerate(h5ad_files):
@@ -52,48 +73,24 @@ class PerturbDataset(Dataset):
         self.cell_barcodes_flattened = np.concatenate([ad.obs_names.tolist() for ad in adatas]).flatten()
 
         # get unique cell types across all h5ad files
-        self.cell_types_flattened = np.concatenate([ad.obs["cell_type"].tolist() for ad in adatas]).flatten()
+        self.cell_types_flattened = np.concatenate([ad.obs[cell_type_label].tolist() for ad in adatas]).flatten()
         # Map categorical labels to integer ids
-        self.cell_types, self.y_index = np.unique(self.cell_types_flattened, return_inverse=True)
-        logger.info(f"Total unique cell types: {len(self.cell_types)}")
+        self.cell_types_onehot_map = make_onehot_encoding_map(np.unique(self.cell_types_flattened))
+        logger.info(f"Total unique cell types: {len(self.cell_types_onehot_map)}")
 
-        # get unique genes across all h5ad files
-        # control label is first, so its one-hot is all zeros
-        self.perturb_vocab_flattened = np.concatenate([ad.var_names.tolist() for ad in adatas])
-        self.perturb_flattened = [] 
-        for adata in adatas:
-            if "guide_id" in adata.obs:
-                self.perturb_flattened = np.concatenate([self.perturb_flattened, adata.obs["guide_id"].tolist()])
-            else:
-                self.perturb_flattened = np.concatenate([self.perturb_flattened, adata.obs["gene"].tolist()])
-        # Map categorical labels to integer ids
-        self.perturbs_training = np.unique(self.perturb_flattened)
-        logger.info(f"Total unique perturbations in training set: {len(self.perturbs_training)}")
-        # TODO: fix this, to make sure that target pertubation is a gene in the vocabulary
-        self.perturbs_vocab = np.unique(self.perturb_vocab_flattened)
-        self.perturbs_vocab = np.concatenate([self.perturbs_vocab, self.perturbs_training])
-        self.perturbs_vocab = np.unique(self.perturbs_vocab)
-        # if the control label is not in the vocabulary, add it
-        if self.control_label not in self.perturbs_vocab:
-            self.perturbs_vocab = np.concatenate([[self.control_label], self.perturbs_vocab])
-
-        logger.info(f"Total perturbations (vocab + training set): {len(self.perturbs_vocab)}")
-
+        self.perturbs_flattened = np.concatenate([ad.obs[target_label].tolist() for ad in adatas]).flatten()
         # get unique batches across all h5ad files
-        self.batches_flattened = np.concatenate([[f"f{i}_"] * ad.n_obs + ad.obs["batch_var"].tolist() for i, ad in enumerate(adatas)]).flatten()
-        self.batches, self.b_index = np.unique(self.batches_flattened, return_inverse=True)
-        logger.info(f"Total unique batches: {len(self.batches)}")
+        self.batches_flattened = np.concatenate([[f"f{i}_"] * ad.n_obs + ad.obs[batch_label].tolist() for i, ad in enumerate(adatas)]).flatten()
+        self.batches_onehot_map = make_onehot_encoding_map(np.unique(self.batches_flattened))
+        logger.info(f"Total unique batches: {len(self.batches_onehot_map)}")
 
         # Index controls by (y,b) for fast lookup
-        self.control_index = np.where(self.perturb_flattened == self.control_label)[0]
+        self.control_index = np.where(self.perturbs_flattened == self.control_label)[0]
         logger.info(f"Total control cells: {len(self.control_index)}")
         self.control_lookup = defaultdict(list)
+        # TODO: support batch strategy
         for i in self.control_index:
-            self.control_lookup[(self.y_index[i], self.b_index[i])].append(i)
-
-        # Store precomputed label tensors
-        self.y = torch.tensor(self.y_index, dtype=torch.long, device=device)
-        self.b = torch.tensor(self.b_index, dtype=torch.long, device=device)
+            self.control_lookup[self.cell_types_flattened[i]].append(i)
 
     def __len__(self):
         return self.n_cells.sum()
@@ -103,51 +100,67 @@ class PerturbDataset(Dataset):
         return np.where(idx < self.n_cells.cumsum())[0][0]
 
     def get_onehot_cell_types(self, idx):
-        return torch.nn.functional.one_hot(idx, num_classes=len(self.cell_types))
+        cell_type = self.cell_types_flattened[idx]
+        return self.cell_types_onehot_map[cell_type]
 
     def get_onehot_perturbs(self, idx):
-        return torch.nn.functional.one_hot(idx, num_classes=len(self.perturbs_vocab))
+        perturb = self.perturbs_flattened[idx]
+        if perturb not in self.pert_embedding:
+            # create all zero embedding
+            self.pert_embedding[perturb] = torch.zeros(next(iter(self.pert_embedding.values())).shape[0])
+        return self.pert_embedding[perturb]
 
     def get_onehot_batches(self, idx):
-        return torch.nn.functional.one_hot(idx, num_classes=len(self.batches))
+        batch = self.batches_flattened[idx]
+        return self.batches_onehot_map[batch]
+    
+    def get_basal_samples(self, idx):
+        # randomly sample n_basal_samples from the control_lookup
+        # return in shape [1, K, G] where K is n_basal_samples
+        cell_type = self.cell_types_flattened[idx]
+        # collect control cells via "random" strategy
+        # TODO: support batch strategy
+        basal_samples_indices = np.random.choice(self.control_lookup[cell_type], size=self.n_basal_samples, replace=True)
+        basal_samples = self.get_x_from_indices(basal_samples_indices)
+        return basal_samples.unsqueeze(0)
 
+    def get_x_from_indices(self, indices):
+        X = torch.tensor([], dtype=torch.float32)
+        for idx in indices:
+            file_idx = self._get_file_idx(idx)
+            adata = self.sp_adatas[file_idx]
+            barcode = self.cell_barcodes_flattened[idx]
+            x = adata.get([barcode])
+            x = torch.tensor(x, dtype=torch.float32)
+            X = torch.cat([X, x])
+
+        return X
+    
     def __getitem__(self, idx):
         # Fetch expression row, convert sparse → dense → torch
         file_idx = self._get_file_idx(idx)
         adata = self.sp_adatas[file_idx]
         barcode = self.cell_barcodes_flattened[idx]
         x = adata.get([barcode])
-        x = torch.tensor(x, dtype=torch.float32, device=self.device)
+        x = torch.tensor(x, dtype=torch.float32)
+        y_onehot = self.get_onehot_cell_types(idx)
+        b_onehot = self.get_onehot_batches(idx)
+        xp_onehot = self.get_onehot_perturbs(idx)
+        x_ctrl_matched = self.get_basal_samples(idx)
 
-        y_scalar = self.y[idx]
-        xp_scalar = torch.tensor(np.where(self.perturb_flattened[idx] == self.perturbs_vocab)[0][0], dtype=torch.long, device=self.device)
-        b_scalar = self.b[idx]
-        y = self.get_onehot_cell_types(y_scalar).float()
-        xp = self.get_onehot_perturbs(xp_scalar).float()
-        b = self.get_onehot_batches(b_scalar)
-
-        # raw number from unique index array, for lookup in control_lookup
-        is_control = self.perturb_flattened[idx] == self.control_label
-
-        # Pick a control matching the perturbation
-        if len(self.control_lookup[(y_scalar, b_scalar)]) > 0:
-            ctrl_idx = np.random.choice(self.control_lookup[(y_scalar, b_scalar)])
-            ctrl_barcode = self.cell_barcodes_flattened[ctrl_idx]
-            x_ctrl_matched = adata.get([ctrl_barcode])
-            x_ctrl_matched = torch.tensor(x_ctrl_matched, dtype=torch.float32, device=self.device)
-        else:
-            x_ctrl_matched = x.clone()
-        return x, y, b, xp, x_ctrl_matched
+        return {
+            "pert_cell_emb": x,
+            "cell_type_onehot": y_onehot,
+            "pert_emb": xp_onehot,
+            "ctrl_cell_emb": x_ctrl_matched,
+            "batch": b_onehot,
+        }
      
 
 if __name__ == "__main__":
     import glob
-    h5ad_files = glob.glob("/home/tphan/state/state/competition_support_set/*.h5")
-    ds = PerturbDataset(h5ad_files)
-    x, y, b, xp, x_ctrl_matched = ds[0]
-    print(x.shape)
-    print(y.shape)
-    print(b.shape)
-    print(xp.shape)
-    print(x_ctrl_matched.shape)
-    # print(ds[228314])
+    h5ad_files = glob.glob("/home/tphan/Softwares/protoplast/notebooks/competition_support_set/*.h5")
+    ds = PerturbDataset(h5ad_files, pert_embedding_file="/home/tphan/Softwares/protoplast/notebooks/competition_support_set/ESM2_pert_features.pt")
+    sample = ds[0]
+    for k, v in sample.items():
+        print(k, v.shape)
--- src/protoplast/scrna/train/utils.py
+++ src/protoplast/scrna/train/utils.py
@@ -1,4 +1,6 @@
 import torch
+import numpy as np
+
 def _to_BD(t):
     # Coerce to (B, G) and contiguous
     if t.ndim == 3 and t.size(1) == 1: t = t.squeeze(1)
@@ -13,3 +15,11 @@ def save_checkpoint(model, optimizer, epoch, model_dir):
         "model_state": model.state_dict(),
         "optimizer_state": optimizer.state_dict(),
     }, f"{model_dir}/epoch={epoch}.pt")
+
+def make_onehot_encoding_map(labels):
+    labels = list(labels)  # make sure it's a list
+    n = len(labels)
+    return {
+        label: torch.tensor(np.eye(n, dtype=int)[i])
+        for i, label in enumerate(labels)
+    }
