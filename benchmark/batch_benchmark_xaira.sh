2acc6ba9d3f84ee47a79f08d4487481ff27d80a2
tri-dataxight <tri@dataxight.com>
2025-09-22 20:18:28 +0800

    batch benchmarking for xaira

---
+++ benchmark/batch_benchmark_xaira.sh
@@ -0,0 +1,14 @@
+
+. .env
+
+# Xaira smallest file
+uv run python benchmark/benchmark.py --class protoplast --label sample --batch-size 1024 "$XAIRA_ROOT_DIR/HCT116_filtered_dual_guide_cells.h5ad"
+uv run python benchmark/benchmark.py --class scdataset --label sample --batch-size 1024 "$XAIRA_ROOT_DIR/HCT116_filtered_dual_guide_cells.h5ad"
+uv run python benchmark/benchmark.py --class scvi --label sample --batch-size 1024 "$XAIRA_ROOT_DIR/HCT116_filtered_dual_guide_cells.h5ad"
+uv run python benchmark/benchmark.py --class annloader --label sample --batch-size 1024 "$XAIRA_ROOT_DIR/HCT116_filtered_dual_guide_cells.h5ad"
+uv run python benchmark/benchmark.py --class anndata --label sample --batch-size 1024 "$XAIRA_ROOT_DIR/HCT116_filtered_dual_guide_cells.h5ad"
+
+# Xaira all files
+uv run python benchmark/benchmark.py --class protoplast --label sample --batch-size 1024 "$XAIRA_ROOT_DIR/*.h5ad"
+uv run python benchmark/benchmark.py --class scdataset --label sample --batch-size 1024 "$XAIRA_ROOT_DIR/*.h5ad"
+uv run python benchmark/benchmark.py --class annloader --label sample --batch-size 1024 "$XAIRA_ROOT_DIR/*.h5ad"
