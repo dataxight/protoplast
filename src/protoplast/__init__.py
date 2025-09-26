0f4391100188453ba52ff3f1fa6c53272921efc5
tri-dataxight <tri@dataxight.com>
2025-09-26 12:12:37 +0800

    tolerate F401 for __init__ file

--- src/protoplast/__init__.py
+++ src/protoplast/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for protoplast."""
 
-from scrna.anndata.trainer import RayTrainRunner
-from scrna.anndata.torch_dataloader import DistributedAnnDataset, DistributedCellLineAnnDataset
 from scrna.anndata.lightning_models import LinearClassifier
+from scrna.anndata.torch_dataloader import DistributedAnnDataset, DistributedCellLineAnnDataset
+from scrna.anndata.trainer import RayTrainRunner
