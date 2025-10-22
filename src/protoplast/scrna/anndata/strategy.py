e0681972d48220aa91d919f9ee8e34b7caa1a4aa
Nam <nam@dataxight.com>
2025-10-22 04:26:38 +0000

    Fix split so that it should take into account batch instead of prefetched-batch when dropping/padding

--- src/protoplast/scrna/anndata/strategy.py
+++ src/protoplast/scrna/anndata/strategy.py
@@ -37,31 +37,71 @@ def ann_split_data(
     is_shuffled: bool = True,
     drop_last: bool = True,
     drop_remainder_warning_pct: float = 0.05,
+    prefetch_factor: int = None,
 ):
-    def to_batches(n):
-        if batch_size >= n:
+    # Adjust batch size based on prefetch factor
+    prefetch_size = batch_size
+    if prefetch_factor:
+        prefetch_size = batch_size * prefetch_factor
+    
+    def to_batches(n) -> list[tuple[int, int]]:
+        if prefetch_size >= n:
             return [(0, n)]
         batches = []
-        for i in range(0, n, batch_size):
-            if i + batch_size > n:
+        for i in range(0, n, prefetch_size):
+            if i + prefetch_size > n:
                 continue
-            batches.append((i, i + batch_size))
+            batches.append((i, i + prefetch_size))
         return batches
 
     def drop_remainder(batches):
+        # Number of samples in all batches
         n = sum(end - start for start, end in batches)
-        remainder = len(batches) % total_workers
+        
+        # Each worker should be assigned equal number of mini-batches
+        # This remainder is the number of extra mini-batches that cannot be evenly
+        # distributed among workers, not the number of prefetched batches
+        remainder = (len(batches) * prefetch_factor) % total_workers
         if remainder > 0:
             if drop_last:
-                dropped_n = sum(end - start for start, end in batches[-remainder:])
+                # Calculate how many prefetched-batch will be dropped because the number of dropped mini-batches
+                # could get larger than prefetch factor
+                dropped_batch = remainder // prefetch_factor
+                dropped_mini_batch = remainder - (dropped_batch * prefetch_factor) # This is always less than prefetch_factor
+                
+                # Drop the full prefetched-batches first, if applied
+                if dropped_batch > 0:
+                    batches = batches[: -dropped_batch]
+                    
+                # Drop the remaining mini-batches from the last prefetched-batch
+                if dropped_mini_batch > 0:
+                    batches[-1] = (batches[-1][0], batches[-1][1] - (dropped_mini_batch * batch_size))
+                
+                dropped_n = remainder * batch_size
                 if dropped_n / n > drop_remainder_warning_pct:
                     logger.warning(f"{dropped_n / n} of data is dropped")
-                batches = batches[:-remainder]
+                
+                
+                # dropped_n = sum(end - start for start, end in batches[-dropped_minibatch:])
+                # if dropped_n / n > drop_remainder_warning_pct:
+                #     logger.warning(f"{dropped_n / n} of data is dropped")
+                # batches = batches[:-dropped_minibatch]
             else:
-                pad = total_workers - remainder
-                logger.info(f"Duplicating data with {pad}")
-                batches.extend(batches[-remainder:] * (pad // remainder))
-                batches.extend(batches[-(pad % remainder) :])
+                # We need to pad *remainder* number of mini-batches so that
+                # total number of mini-batches is divisible by total_workers
+                added_batch = remainder // prefetch_factor
+                added_mini_batch = remainder - (added_batch * prefetch_factor)
+                
+                logger.info("Duplicating data")
+                if added_batch > 0:
+                    batches.extend(batches[-added_batch:])
+                if added_mini_batch > 0:
+                    batches.append((batches[-1][0], batches[-1][0] + (added_mini_batch * batch_size),))
+                
+                # pad = total_workers - remainder
+                # logger.info(f"Duplicating data with {pad}")
+                # batches.extend(batches[-remainder:] * (pad // remainder))
+                # batches.extend(batches[-(pad % remainder) :])
         return batches
 
     if not rng:
@@ -78,9 +118,9 @@ def ann_split_data(
             metadata_cb(ad, metadata)
 
         n_obs = ad.n_obs
-        if batch_size > n_obs:
+        if prefetch_size > n_obs:
             logger.warning(
-                f"Batch size ({batch_size}) is greater than number of observations "
+                f"Batch size ({prefetch_size}) is greater than number of observations "
                 f"in file {fp} ({n_obs}). Only one batch will be created.",
                 stacklevel=2,
             )
@@ -313,13 +353,9 @@ class SequentialShuffleStrategy(ShuffleStrategy):
         self.drop_last = drop_last
 
     def split(self) -> SplitInfo:
-        if self.pre_fetch_then_batch:
-            batch_size = self.batch_size * self.pre_fetch_then_batch
-        else:
-            batch_size = self.batch_size
         split_dict = ann_split_data(
             self.file_paths,
-            batch_size,
+            self.batch_size,
             self.total_workers,
             self.test_size,
             self.validation_size,
@@ -327,6 +363,7 @@ class SequentialShuffleStrategy(ShuffleStrategy):
             self.metadata_cb,
             self.is_shuffled,
             self.drop_last,
+            prefetch_factor=self.pre_fetch_then_batch,
         )
         # this will be passed to the dataset, inorder to know the mini batch size
         if self.pre_fetch_then_batch:
